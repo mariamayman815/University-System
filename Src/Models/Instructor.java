78ffa4b1939e54257e84950b2b260a87bd8864d5
mariam ayman <mariamayman23hh@gmail.com>
2025-12-03 08:37:17 +0200

    Models Student Features

---
+++ Src/Models/Instructor.java
@@ -0,0 +1,36 @@
+package Models;
+import java.util.ArrayList;
+
+public class Instructor extends User{
+    private int officeNumber;
+    private ArrayList<String> coursesTaught;
+
+//    Default constructor
+    public Instructor() {
+        super();
+        coursesTaught = new ArrayList<>();
+        officeNumber = 0;
+    }
+//    Parameterized constructor
+    public Instructor(User user,int officeNumber,ArrayList<String> coursesTaught){
+        super(user);
+        this.officeNumber=officeNumber;
+        this.coursesTaught=coursesTaught;
+    }
+//    Setters & Getters
+    public int getOfficeNumber() {
+        return officeNumber;
+    }
+
+    public void setOfficeNumber(int officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+    public ArrayList<String> getCoursesTaught() {
+        return coursesTaught;
+    }
+
+    public void setCoursesTaught(ArrayList<String> coursesTaught) {
+        this.coursesTaught = coursesTaught;
+    }
+}
