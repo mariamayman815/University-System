792fbb4ae1043ee5e34cd552be8e7374f6a4efca
mariam ayman <abmnm85@gmail.com>
2025-12-07 18:39:53 +0200

    before merge

---
+++ Src/Student-features/Services/StudentService.java
@@ -0,0 +1,8 @@
+Student class {
+    
+    public boolean logIn(String password,String userName){
+        if(this.password.equal(password)&&this.userName.equal(userName)) return true;
+        else return 0;
+    }
+
+}
